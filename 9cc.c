228cdd37e64bab7eafa5aa5da50158aec99008ed
mamezou <mamezou.king@gmail.com>
2021-05-16 13:32:38 +0900

    step4まで

--- 9cc.c
+++ 9cc.c
@@ -136,3 +136,22 @@ int main(int argc, char **argv) {
   printf("  ret\n");
   return 0;
 }
+
+// input program
+char *user_input;
+
+// errorを報告
+void error_at(char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  int pos = loc - user_input;
+  fprintf(stderr, "%s/n", user_input);
+  fprintf(stderr, "%*s", pos, " ");
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "/n");
+  exit(1);
+}
+
+
+
